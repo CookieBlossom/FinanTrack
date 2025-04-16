5f446b1ecc71127d9f469d821acd34580f43d127
Blossom <mariajesuss2424@gmail.com>
2025-04-15 22:49:04 -0400

    routes fixed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -11,9 +12,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([{ path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'dashboard', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) }, { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) }, { path: 'register', loadChildren: () => import('./modules/register/register.module').then(m => m.RegisterModule) }, { path: 'profile', loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule) }, { path: 'card', loadChildren: () => import('./modules/card/card.module').then(m => m.CardModule) }, { path: 'analytics', loadChildren: () => import('./modules/analytics/analytics.module').then(m => m.AnalyticsModule) }]) // Configuración de rutas vacía por ahora
-  ],
+    RouterModule.forRoot(routes) ],
   providers: [],
-  bootstrap: [AppComponent] // Componente raíz que inicia la app
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./modules/login/login.module').then(m => m.LoginModule),
+  },
+  {
+    path: 'register',
+    loadChildren: () =>
+      import('./modules/register/register.module').then(m => m.RegisterModule),
+  },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('./modules/profile/profile.module').then(m => m.ProfileModule),
+  },
+  {
+    path: 'card',
+    loadChildren: () =>
+      import('./modules/card/card.module').then(m => m.CardModule),
+  },
+  {
+    path: 'analytics',
+    loadChildren: () =>
+      import('./modules/analytics/analytics.module').then(m => m.AnalyticsModule),
+  },
+  // Redirección por defecto
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  // Ruta para manejar páginas no encontradas
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
