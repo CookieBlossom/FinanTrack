7c21ed7f8229ca14f714412abfa4de1d3e0a9e19
caritofm <163791730+caritofm@users.noreply.github.com>
2025-04-17 00:56:45 -0400

    los inputs estan listos solo me falta cuadrar el circulo en la pagina de registro

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { routes } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent // Declaración del componente principal
+  ],
+  imports: [
+    
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes) ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
