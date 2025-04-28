04b8db58a93751d64982879378888531a221a87c
caritofm <163791730+caritofm@users.noreply.github.com>
2025-04-28 18:27:47 -0400

    pagina profile en proceso

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { routes } from './app.routes';
+import {MatIconModule} from '@angular/material/icon';
+import { ProfileComponent } from './modules/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent // Declaración del componente principal
+    AppComponent, // Declaración del componente principal
+    ProfileComponent
   ],
   imports: [
+    MatIconModule,
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(routes) ],
--- frontend/src/app/modules/profile/profile.modules.ts
+++ frontend/src/app/modules/profile/profile.modules.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatIconModule, MatButtonModule],
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfileComponent {}
