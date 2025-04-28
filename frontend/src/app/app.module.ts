1df6b36c1a1708a2428ecdce871061f2f2edae61
Blossom <mariajesuss2424@gmail.com>
2025-04-28 18:48:37 -0400

    modified profile config

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,12 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { routes } from './app.routes';
-import {MatIconModule} from '@angular/material/icon';
-import { ProfileComponent } from './modules/profile/profile.component';
-
 @NgModule({
-  declarations: [
-    AppComponent, // Declaración del componente principal
-    ProfileComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    MatIconModule,
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(routes) ],
--- frontend/src/app/modules/profile/profile.modules.ts
+++ frontend/src/app/modules/profile/profile.modules.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, RouterModule, MatIconModule, MatButtonModule, MatIcon],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
