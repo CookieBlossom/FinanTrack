0d49aee472bde7ef990306402dcc23c718f828bc
caritofm <163791730+caritofm@users.noreply.github.com>
2025-04-27 23:08:17 -0400

    login listo

--- frontend/src/app/modules/login/login.component.ts
+++ frontend/src/app/modules/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
-  standalone: false,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- frontend/src/app/modules/register/register.modules.ts
+++ frontend/src/app/modules/register/register.modules.ts
@@ -10,4 +10,21 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+
+  nombre: string = "";
+  email: string = "";
+  telefono: string = "";
+  password: string = "";
+  passwordConf :string = "";
+
+  GuardarDatos( ){
+    console.log('Guardando datos')
+    if(!this.nombre || !this.email || !this.telefono || !this.password ||!this.passwordConf){
+      alert('Porfavor complete todos los campos')
+      return
+    }
+  
+  }
+}
+
