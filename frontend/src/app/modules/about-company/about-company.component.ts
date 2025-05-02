eae05d3b760f6ee7ddd07926259fa215b16d670e
caritofm <163791730+caritofm@users.noreply.github.com>
2025-05-02 01:21:40 -0400

    about-company casi ok

---
+++ frontend/src/app/modules/about-company/about-company.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+
+@Component({
+  selector: 'app-about-company',
+  imports: [ MatIcon],
+  templateUrl: './about-company.component.html',
+  styleUrl: './about-company.component.css'
+})
+export class AboutCompanyComponent {
+
+}
