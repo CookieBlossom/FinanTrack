372561ab2156f88097a3e57c6e1be2ad98845b48
Blossom <mariajesuss2424@gmail.com>
2025-05-04 21:58:39 -0400

    finished subscriptions

--- frontend/src/app/modules/subscriptions/subscriptions.modules.ts
+++ frontend/src/app/modules/subscriptions/subscriptions.modules.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from '../../shared/shared.modules';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-subscriptions',
   standalone: true,
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './subscriptions.component.html',
   styleUrls: ['./subscriptions.component.scss']
 })
