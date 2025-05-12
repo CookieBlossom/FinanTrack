cde01afa6c8921bb5fd86b888cd296942dd6630b
caritofm <163791730+caritofm@users.noreply.github.com>
2025-05-12 00:09:32 -0400

    frontend ok

--- frontend/src/app/modules/register/register.component.ts
+++ frontend/src/app/modules/register/register.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
+  imports:[RouterModule]
 })
 export class RegisterComponent {
   
